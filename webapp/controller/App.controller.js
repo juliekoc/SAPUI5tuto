6ca98e8ff946d66f45c7e054f3b853b2d6bbb611
juliekoc <julie.kocianova@planisense.com>
2022-09-01 16:05:27 +0200

    step 15 done: nested views made

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,19 +1,7 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast", /*Imports message boxgit*/
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast) {
-   "use strict"; /*makes sure variables ae declared before they're used*/
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-   /*Extends Controller object of SAPUI5 core*/
-      onShowHello : function () {
-           // read msg from i18n model
-           var oBundle = this.getView().getModel("i18n").getResourceBundle();
-           var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-           var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-           // show message
-           MessageToast.show(sMsg);
-      }
    });
 });
