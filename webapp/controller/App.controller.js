ce6d6ccbb3477e23a23d608fdf062e7f9bdb1a88
juliekoc <julie.kocianova@planisense.com>
2022-09-01 09:54:43 +0200

    step 6 done : displays a message box

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast" /*Imports message box*/
+], function (Controller, MessageToast) {
    "use strict"; /*makes sure variables ae declared before they're used*/
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
    /*Extends Controller object of SAPUI5 core*/
       onShowHello : function () {
          // show a native JavaScript alert
-         alert("Hello World");
+         MessageToast.show("Hello World");
       }
    });
 });
