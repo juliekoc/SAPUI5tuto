244ac4b6994337451070fba5824726e1a357beec
juliekoc <julie.kocianova@planisense.com>
2022-09-01 09:48:51 +0200

    step 5 done

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller"
 ], function (Controller) {
-   "use strict";
+   "use strict"; /*makes sure variables ae declared before they're used*/
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+   /*Extends Controller object of SAPUI5 core*/
       onShowHello : function () {
          // show a native JavaScript alert
          alert("Hello World");
