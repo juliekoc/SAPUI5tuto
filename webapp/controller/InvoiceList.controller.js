49dee3029df47904e4d41c5c5e973c2048ec8390
juliekoc <julie.kocianova@planisense.com>
2022-09-02 17:57:42 +0200

    alert creation in progress

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,35 +1,42 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/Fragment",
 	"../model/formatter",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator"
-], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+], function (Controller, MessageToast, Fragment) {
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
+	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+	    formatter: formatter,
 
-		formatter: formatter,
-		onInit : function () {
-			var oViewModel = new JSONModel({
-				currency: "EUR"
+		onOpenDialog : function () {
+
+			// create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = this.loadFragment({
+					name: "sap.ui.demo.walkthrough.view.AlertDialog"
+				});
+			}
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
 			});
-			this.getView().setModel(oViewModel, "view");
 		},
-		onFilterInvoices : function (oEvent) {
+         		onFilterInvoices : function (oEvent) {
 
-        			// build filter array
-        			var aFilter = [];
-        			var sQuery = oEvent.getParameter("query");
-        			if (sQuery) {
-        				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
-        			}
+         			// build filter array
+         			var aFilter = [];
+         			var sQuery = oEvent.getParameter("query");
+         			if (sQuery) {
+         				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+         			}
 
-        			// filter binding
-        			var oList = this.byId("invoiceList");
-        			var oBinding = oList.getBinding("items");
-        			oBinding.filter(aFilter);
-        		}
+         			// filter binding
+         			var oList = this.byId("invoiceList");
+         			var oBinding = oList.getBinding("items");
+         			oBinding.filter(aFilter);
+         		}
+         	});
 
 	});
 });
